ce6d415e95755d358207059da3208a3fbdc5826d
vavu786 <hamzamian.patwa@gmail.com>
2020-12-22 20:37:00 -0600

    Home page stays index.html and javascript updates it. Added yeti animation.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,46 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
+function changeToYeti () {
+    document.getElementById("content").innerHTML = "";
+    $( "#content" ).load( "yeti.html" );
+}
+
+function changeToBigfoot () {
+    document.getElementById("content").innerHTML = "";
+    $( "#content" ).load( "bigfoot.html" );
+}
+
+function changeToLochness () {
+    document.getElementById("content").innerHTML = "";
+    $( "#content" ).load( "lochness.html" );
+}
+
+function changeToChupacabra () {
+    document.getElementById("content").innerHTML = "";
+    $( "#content" ).load( "chupacabra.html" );
+}
+
+function changeToHome () {
+    document.getElementById("content").innerHTML = "";
+    $( "#content" ).load( "index.html" );
+}
+
+async function animateYeti () {
+    let yeti_sprite = document.querySelector("#animatedYeti");
+    yeti_sprite.style.width = "1176px";
+    yeti_sprite.style.height = "1168px";
+    yeti_sprite.style.transform = "scale(0.10,0.10)";
+    yeti_sprite.style.borderRadius = "300px";
+    let images = ["url('../images/yeti_sprite_1.jpg')", "url('../images/yeti_sprite_2.jpg')",
+                  "url('../images/yeti_sprite_3.jpg')", "url('../images/yeti_sprite_4.jpg')",
+                  "url('../images/yeti_sprite_5.jpg')", "url('../images/yeti_sprite_6.jpg')",
+                  "url('../images/yeti_sprite_7.jpg')"];
+
+    for (let i = 0; i < 280; i++) {
+        yeti_sprite.style.backgroundImage = images[i%7];
+        await sleep(50);
+    }
+
+}
